ca7b725b85a24ec6164c18958cc871f562a31717
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-01-25 17:40:29 -0500

    updates to ensure state is persisted on page refresh. using redux-persist npm package

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import store from "./store/index";
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from "./store";
 import App from "./App.js";
 
 render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   // The target element might be either root or app,
   // depending on your development environment
