cc095f195471ac24ed2388669b6a0a20262d8d82
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-01-25 13:51:46 -0500

    removing switch statement as each reducer performs specific tasks

--- src/js/reducers/add-article.js
+++ src/js/reducers/add-article.js
@@ -2,14 +2,10 @@ import { ADD_ARTICLE } from "../constants/action-types";
 import initialState from "../reducers/initial-state"
 
 const addArticleReducer = (state = initialState, action) => {
-    switch(action.type) {
-        case ADD_ARTICLE:
-            return Object.assign({}, state, {
-                articles: state.articles.concat(action.article)
-            });
-        default:
-            return state;
+    if(action.type === ADD_ARTICLE) {
+        return Object.assign({}, state, {articles: state.articles.concat(action.article)});
     }
+    return state;
 }
 
 export default addArticleReducer;
