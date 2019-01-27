3dc975f45e4e8f09bb369ac868326322c869eae8
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-01-26 23:46:04 -0500

    making available the action to add new task to top of the list

--- src/reducers/task.js
+++ src/reducers/task.js
@@ -5,6 +5,8 @@ export const taskReducer = (state = initialState, action) => {
     switch(action.type) {
         case ADD_TASK:
             return Object.assign({}, state, {tasks: state.tasks.concat(action.task)});
+            // to concat at top of the list, use below
+            //return Object.assign({}, state, {tasks: [action.task].concat(state.tasks)});
         case REMOVE_TASK:
             //https://stackoverflow.com/questions/15287865/remove-array-element-based-on-object-property    
             return Object.assign({}, state, {tasks: state.tasks.filter((task) => {return task.id !== action.taskId})})
