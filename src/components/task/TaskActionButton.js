42ed8bddba436c24d27316221ffd82897521a0c1
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-02-08 14:53:45 -0500

    getting rid of unused taskButtonClick function and useless constructor

--- src/components/task/TaskActionButton.js
+++ src/components/task/TaskActionButton.js
@@ -2,15 +2,6 @@ import React, { Component } from "react";
 import Button from '../Button';
 
 class TaskActionButton extends Component {
-    constructor(props) {
-        super(props);
-        this.taskButtonClick = this.taskButtonClick.bind(this);
-    }
-
-    taskButtonClick() {
-        this.props.onClick(this.props.task);
-    }
-
     render() {
         return (
             <Button btnTxt={this.props.btnTxt} btnClass={this.props.btnClass} disabled={this.props.disabled} onClick={this.props.onClick} />
