94ce3ab9d82e81386a269e340040d59894db9264
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-02-09 17:12:23 -0500

    removing Object.assign and using spread syntax

--- src/reducers/task.js
+++ src/reducers/task.js
@@ -10,12 +10,18 @@ const initialState = {"tasks": []};
 export const taskReducer = (state = initialState, action) => {
     switch(action.type) {
         case ADD_TASK:
-            return Object.assign({}, state, {tasks: state.tasks.concat(action.task)});
+            return {...state, tasks: state.tasks.concat(action.task)};
             // to concat at top of the list, use below
-            //return Object.assign({}, state, {tasks: [action.task].concat(state.tasks)});
+            //return {...state, tasks: [action.task].concat(state.tasks)};
         case REMOVE_TASK:
             //https://stackoverflow.com/questions/15287865/remove-array-element-based-on-object-property    
-            return Object.assign({}, state, {tasks: state.tasks.filter((task) => {return task.id !== action.taskId})});
+            //return Object.assign({}, state, {tasks: state.tasks.filter((task) => {return task.id !== action.taskId})});
+            return {
+                ...state, 
+                tasks: state.tasks.filter(
+                    (task) => { return task.id !== action.taskId  }
+                )
+            };
         case REMOVE_ALL_TASKS:
             return Object.assign({}, state, {tasks: []});
         case COMPLETE_TASK:
@@ -24,7 +30,7 @@ export const taskReducer = (state = initialState, action) => {
                 tasks: state.tasks.map(
                     (task) => task.id === action.taskId ? {...task, new: false, completed: true} : task
                 )
-            }
+            };
         default:
             return state;
     }
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -6,8 +6,8 @@ const intitialState = {"user": null};
 export const userReducer = (state = intitialState, action) => {
     switch(action.type) {
         case ADD_SESSION_USER:
-            return Object.assign({}, state, {user: action.user});
-            //return {...state, user: action.user};
+            //return Object.assign({}, state, {user: action.user});
+            return {...state, user: action.user};
             //return state;
         default:
             return state
