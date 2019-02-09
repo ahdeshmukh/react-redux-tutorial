9a2d62e483e5cfb5d83be6b36d89b755d9c8395b
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-02-09 18:10:32 -0500

    dispatching multipe actions from the middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,10 +1,14 @@
-import { REMOVE_ALL_TASKS, CLEAR_STATE } from "../constants/action-types"
+import { REMOVE_ALL_TASKS, CLEAR_STATE, REMOVE_SESSION_USER } from "../constants/action-types"
 
 export const clearStateMiddleware = ({dispatch}) => {
     return function(next) {
         return function(action) {
             if (action.type === CLEAR_STATE) {
-                return dispatch({ type: REMOVE_ALL_TASKS });
+                const actions = [REMOVE_ALL_TASKS, REMOVE_SESSION_USER];
+                //return dispatch({ type: REMOVE_ALL_TASKS });
+                actions.map(action => {
+                    return dispatch({type: action});
+                })
             }
             return next(action);
         };
