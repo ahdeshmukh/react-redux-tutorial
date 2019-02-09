6ca1896e5b78e2b20d13df4b96a484f25fe13b15
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-02-09 18:09:57 -0500

    adding REMOVE_SESSION_USER to the userReducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,4 +1,4 @@
-import { ADD_SESSION_USER } from "../constants/action-types";
+import { ADD_SESSION_USER, REMOVE_SESSION_USER } from "../constants/action-types";
 //import initialState from "./initial-state"
 
 const intitialState = {"user": null};
@@ -9,6 +9,8 @@ export const userReducer = (state = intitialState, action) => {
             //return Object.assign({}, state, {user: action.user});
             return {...state, user: action.user};
             //return state;
+        case REMOVE_SESSION_USER:
+            return intitialState;
         default:
             return state
     }
