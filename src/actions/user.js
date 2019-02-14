0889a9aeafd8be9419ee5923f3ba85172c948d92
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-02-13 22:43:29 -0500

    adding removeSessionUser action creator

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,5 +1,9 @@
-import { ADD_SESSION_USER } from "../constants/action-types";
+import { ADD_SESSION_USER, REMOVE_SESSION_USER } from "../constants/action-types";
 
 export function addSessionUser(user) {
     return { type: ADD_SESSION_USER, user };
+};
+
+export function removeSessionUser() {
+    return { type: REMOVE_SESSION_USER };
 };
